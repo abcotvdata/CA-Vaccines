dfa9cf1417712f63087933493bfadabe03a0c1ae
lindseyf96 <39309451+lindseyf96@users.noreply.github.com>
2022-05-20 19:29:55 -0700

    Update upload-covid-by-vaccine-status.R

--- scripts/upload-covid-by-vaccine-status.R
+++ scripts/upload-covid-by-vaccine-status.R
@@ -1,1 +1,10 @@
+library(knitr)
+library(markdown)
+library(rmarkdown)
+
+## knitr loop
+
+render('~/R/CA-Vaccines/covid-19-cases-by-vaccination-status.Rmd', output_file="covid-19-cases-by-vaccination-status.html")
+render('~/R/CA-Vaccines/covid-19-hosp-by-vaccination-status.Rmd', output_file="covid-19-hosp-by-vaccination-status.html")
+render('~/R/CA-Vaccines/covid-19-deaths-by-vaccination-status.Rmd', output_file="covid-19-deaths-by-vaccination-status.html")
 
